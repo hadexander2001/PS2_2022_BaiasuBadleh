f1270a17ee91c1d95ae4c70b0bf69a488c6a1b9d
Hady Badleh <hadexander2001@gmail.com>
2022-05-09 22:56:02 +0300

    Improvements

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, render_template
+from flask import request
 import serial
 app = Flask(__name__)
 
@@ -7,19 +9,7 @@ print(ser.name)
 
 @app.route('/')
 def main_interface():
-    text = 'Baiasu&Badleh 2022'
-    temp = ' - Temperature is currently unavailable'
-    # temp_serial = ser.readlines()
-
-    buttonTags = \
-        '<p>\
-        <button onclick="document.location=\'led_off\'">LED OFF</button> \
-        <button onclick="document.location=\'led_on\'">LED ON</button> \
-        <button onclick="document.location=\'set_rgb_led\'">RGB LED</button> \
-        <button onclick="document.location=\'EEPROMmessages\'">EEPROM Messages</button> \
-        </p>'
-        
-    return text + temp  + buttonTags #+temp_serial.decode()
+    return render_template('home.html')
     
 @app.route('/led_on')
 def led_on():
@@ -31,12 +21,22 @@ def led_off():
     ser.write("1 S".encode())
     return "LED OFF"
 
-# Improvable
-@app.route('/set_rgb_led')
+@app.route('/set_rgb_led', methods=['GET'])
+def RGBScalingInterface():
+    return render_template('set_rgb_led.html')
+
+@app.route('/set_rgb_led', methods=['POST'])
 def set_rgb_led():
-    ser.write("2 FF0000".encode())
-    return "RGB LED SET TO RED"
+    red = request.form['RED']
+    green = request.form['GREEN']
+    blue = request.form['BLUE']
+    print("2 " + red + green + blue)
+    ser.write(("2 " + red + green + blue).encode())
+    return render_template('set_rgb_led.html')
 
 @app.route('/EEPROMmessages')
 def readEEPROM():
     return "Currently Unavailable"
+
+if __name__ == '__main__':
+    app.run()
